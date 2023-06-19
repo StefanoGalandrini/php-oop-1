78b20e2eee78c56a3980b2deccf3cca35a04c533
StefanoGalandrini <stefe68@gmail.com>
2023-06-19 17:24:08 +0200

    Move class definition in file movie.php

---
+++ models/movie.php
@@ -0,0 +1,61 @@
+<?php
+
+class Movie
+{
+	public string $title;
+	public string $director;
+	public string $country;
+	public array $genres;
+	public array $cast;
+	public int $year;
+	public ?string $poster;
+
+	public function __construct(
+		string $title,
+		string $director,
+		string $country,
+		array $genres,
+		array $cast,
+		int $year,
+		?string $poster
+	) {
+		$this->title = $title;
+		$this->director = $director;
+		$this->country = $country;
+		$this->genres = $genres;
+		$this->cast = $cast;
+		$this->year = $year;
+		$this->poster = $poster;
+	}
+
+	public function getPosterPath($filename)
+	{
+		$fullPath = "img/" . $filename;
+		return $fullPath;
+	}
+
+	public function displayMovieInfo()
+	{
+		echo "<h2>{$this->title}</h2>";
+		echo "<p>Director: {$this->director}</p>";
+		echo "<p>Country: {$this->country}</p>";
+		echo "<p>Genres:</p>";
+		echo "<ul>";
+		foreach ($this->genres as $genre) {
+			echo "<li>$genre</li>";
+		}
+		echo "</ul>";
+		echo "<p>Cast:</p>";
+		echo "<ul>";
+		foreach ($this->cast as $actor) {
+			echo "<li>$actor</li>";
+		}
+		echo "</ul>";
+		echo "<p>Year: {$this->year}</p>";
+		if ($this->poster) {
+			$posterPath = $this->getPosterPath($this->poster);
+			echo "<p>{$posterPath}</p>";
+			// echo "<img src='{$posterPath}' alt='{$this->title} Poster'>";
+		}
+	}
+}
