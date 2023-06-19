8633c35b64f86565277fa38d6d27e1edec5e9f5f
StefanoGalandrini <stefe68@gmail.com>
2023-06-19 18:40:10 +0200

    Add initial layout for film cards

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <?php
-
 include __DIR__ . "/models/movie.php";
 
 $json = file_get_contents(__DIR__ . "/db.json");
 $moviesData = json_decode($json, true);
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP Movies</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-foreach ($moviesData as $movie) {
-    $movie = new Movie(
-        $movie['title'],
-        $movie['director'],
-        $movie['country'],
-        $movie['genres'],
-        $movie['cast'],
-        $movie['year'],
-        $movie['poster']
-    );
+<body>
+    <div class="container">
+        <div class="movie-grid">
+            <?php
+            foreach ($moviesData as $movieData) {
+                $movie = new Movie(
+                    $movieData['title'],
+                    $movieData['director'],
+                    $movieData['country'],
+                    $movieData['genres'],
+                    $movieData['cast'],
+                    $movieData['year'],
+                    $movieData['poster']
+                );
+                echo '<div class="movie-card">';
+                $movie->displayMovieInfo();
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </div>
+</body>
 
-    $movie->displayMovieInfo();
-}
+</html>
